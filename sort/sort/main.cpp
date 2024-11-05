29e623984de476e97e0881b7b8f365695280c386
Pierre-Louis COPPENS <pierre-louis.coppens@etu.minesparis.psl.eu>
2024-11-05 16:58:28 +0100

    Initialisation du repo et sort

---
+++ sort/sort/main.cpp
@@ -0,0 +1,22 @@
+//Premier programme de tri sur CPP
+
+#include <iostream>
+int main(){
+    std::cout << "my sort algorithm" << std::endl;
+    int tab[] = {1,4,3,5,2,6}; //taille du tableau : 6
+    for (int i = 0; i < 7; i = i + 1) {
+        int max = tab[i];
+        int jmax = i;
+        for (int j = i; j < 7; j = j + 1){
+            if (tab[j] < max){
+                max = tab[j];
+                jmax = j;
+            }}
+        int inter = tab[jmax];
+        tab[jmax] = tab[i];
+        tab[i] = inter;
+    }
+    for (int i = 0; i < sizeof(tab) / sizeof(tab[0]); i++) {
+        std::cout << tab[i] << std::endl;
+    }
+}
